3f4ab5d1067466d33421eb39d0251bcffecc64b6
Shreyash <91046521+hellobro12@users.noreply.github.com>
2025-09-10 00:24:38 +0530

    Update Statesofmatter.jsx

--- src/Statesofmatter.jsx
+++ src/Statesofmatter.jsx
@@ -1,4 +1,4 @@
-// imports 
+// imports react canva box vector
 import React, { useState, useRef } from 'react';
 import { Canvas, useFrame, useThree } from '@react-three/fiber';
 import { Box as BoxMesh } from '@react-three/drei';
